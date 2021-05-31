437deebcdd80db81f9c7284b9a8ca4f3b4c7b60c
Rabia Canpolat <66038847+rcanpolaat@users.noreply.github.com>
2021-05-31 23:14:50 +0300

    Update LoginPage.dart

--- scannerapplication/lib/LoginPage.dart
+++ scannerapplication/lib/LoginPage.dart
@@ -57,8 +57,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        title: Image.asset('assets/images/purplelogo.png', fit : BoxFit.contain, height: 72,),
+        //title: Image.asset('assets/images/purplelogo.png', fit : BoxFit.contain, height: 72,),
         centerTitle: true,
       ),
       body: Padding(
@@ -68,19 +69,22 @@ class _LoginPageState extends State<LoginPage> {
             Container(
               alignment: Alignment.center,
               padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-              child: Image.asset('assets/images/scanrcpt.png',width: 150,height: 70,)
+              child: Image.asset('assets/images/scanrcpt.png',width: 100,height: 100,)
             ),
             Container(
               alignment: Alignment.center,
               padding: EdgeInsets.fromLTRB(50, 0, 50, 20),
-              child: Image.asset('assets/images/scan.png',width: 60,height: 60,)
+              child: Image.asset('assets/images/scan.png',width: 80,height: 80,)
             ),
+            SizedBox(height:60),
             Container(
               padding: EdgeInsets.fromLTRB(50, 10, 50, 20),
               child: TextField(
+                cursorColor: Colors.purple,
                 controller: mail,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
+                  icon: Icon(Icons.mail,size: 20),
                   labelText: "E-Mail",
                 ),
               ),
@@ -92,29 +96,38 @@ class _LoginPageState extends State<LoginPage> {
                 controller: password,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
+                  icon: Icon(Icons.vpn_key_outlined,size: 20),
                   labelText: 'Password',
                 ),
               ),
             ),
+            SizedBox(height: 100),
             Container(
               alignment: Alignment.center,
-                child: 
-                RaisedButton(
-                    padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-                    color: Colors.purple,
-                    textColor: Colors.white,
-                    child: Text("Log in"),
-                    onPressed: girisYap),
+                child:
+                Row(
+              
+                  children: [
+                     Text('                               '),
+                    IconButton(
+                      icon: Icon(Icons.login),iconSize: 50, color: Colors.purple,
+                        onPressed: girisYap),
+                        Text('Login',style: TextStyle(color: Colors.purple,fontSize: 20))
+                  ],
+                ),
             ),
             Container(
               alignment: Alignment.center,
                 child: 
-                RaisedButton(
-                    padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-                    color: Colors.purple,
-                    textColor: Colors.white,
-                    child: Text("Sign up"),
+                Row(
+                 
+                  children: [
+                    Text('                                 '),
+                    IconButton(icon: Icon(Icons.data_saver_on),iconSize: 50,color: Colors.purple,
                     onPressed: kayitOl),
+                    Text('Sign Up',style: TextStyle(color: Colors.purple,fontSize: 20))
+                  ],
+                ),
             ),
           ],
         ),
